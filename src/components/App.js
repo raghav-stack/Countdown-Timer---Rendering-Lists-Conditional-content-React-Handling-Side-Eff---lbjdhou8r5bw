c1017c177f672f40b4180952627221bc2d67b13f
Raghav <kumarraghav71950@gmail.com>
2023-03-29 15:10:56 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,44 @@
-import React, { Component, useState, useRef } from "react";
+import React, { Component, useState, useEffect } from "react";
 import '../styles/App.css';
 
 const App = () => {
   // write your code here 
-  let timerRef = null;
-  const rt = useRef(0);
-  const [remaingTime,setRemainingTime] = useState(0);
-  
-  const getTimeRemaining = (e) => {
+  const [counter, setCounter] = useState(0)
 
-    const total = Date.parse(e) - Date.parse(new Date());
-    console.log(e.getSeconds(), new Date().getSeconds());
-    
-    const seconds = Math.floor((total / 1000) % 60);
-    const minutes = Math.floor((total / 1000 / 60) % 60);
-    const hours = Math.floor((total / 1000 / 60 / 60) % 24);
-    return {
-        total, hours, minutes, seconds
+  var changeCounter = (e) => {
+    if (e.key === "Enter") {
+      if (isNaN(e.target.value) || e.target.value === '') {
+        setCounter(0);
+      } else {
+        setCounter(parseInt(e.target.value));
+      }
+    }
+  };
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      if (counter === 0) {
+        setCounter(0);
+      }
+      if (counter !== 0) {
+        setCounter(counter - 1);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(i);
     };
-  }
-  
-  const startCount = (time) => {
-    let { total, hours, minutes, seconds }  = getTimeRemaining(time);
-    console.log(time)
-    if(total>=0){
-      setRemainingTime(remaingTime-1)
-      rt.current.innerText = rt.current.innerText-1;
-    }else{
-      clearInterval(timerRef)
-    }
-  }
-  
-  const startCountDownHandle = (event) => {
-    if(event.target.value>0 && event.key == 'Enter'){
-      console.log("start")
-      rt.current.innerText = event.target.value;
-      const t = parseInt(event.target.value);
-      let deadline = new Date();
-      deadline.setSeconds(deadline.getSeconds() + t);
-      if(timerRef)
-        clearInterval(timerRef);
-      timerRef = setInterval(() => {
-        
-        startCount(deadline);
-      }, 1000)
-    }
-  }
-  
+  }, [counter]);
+
+
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={startCountDownHandle} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={changeCounter} /> sec.
         </h1>
       </div>
-      <div id="current-time" ref={rt}></div>
+      <div id="current-time">{counter}</div>
     </div>
   )
 }
